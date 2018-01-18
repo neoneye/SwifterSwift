d00f03f9debebde566db7666c594125b85907b13
Simon Strandgaard <neoneye@gmail.com>
2018-01-18 00:59:23 +0000

    Removed `Array.divided(by:)`

--- Tests/SwiftStdlibTests/ArrayExtensionsTests.swift
+++ Tests/SwiftStdlibTests/ArrayExtensionsTests.swift
@@ -291,20 +291,6 @@ final class ArrayExtensionsTests: XCTestCase {
         XCTAssertEqual(grouped["j"] ?? [], [ "james", "jordan", "jonshon" ])
         XCTAssertEqual(grouped["i"] ?? [], [ "irving", "iverson" ])
     }
-    
-	func testDivided() {
-		let input = [0, 1, 2, 3, 4, 5]
-		let (even, odd) = input.divided { $0 % 2 == 0 }
-		XCTAssertEqual(even, [0, 2, 4])
-		XCTAssertEqual(odd, [1, 3, 5])
-
-		// Parameter names + indexes
-		let tuple = input.divided { $0 % 2 == 0 }
-		XCTAssertEqual(tuple.matching, [0, 2, 4])
-		XCTAssertEqual(tuple.0, [0, 2, 4])
-		XCTAssertEqual(tuple.nonMatching, [1, 3, 5])
-		XCTAssertEqual(tuple.1, [1, 3, 5])
-	}
 
     func testForEachSlice() {
         var iterations: Int = 0
